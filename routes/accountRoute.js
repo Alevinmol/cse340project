021fb6ddf38e08d54512207645564f7c773650da
Alevinmol <vinay2@byui.edu>
2024-12-12 20:45:55 -0600

    login

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -4,6 +4,10 @@ const router = new express.Router()
 const utilities = require("../utilities/")
 const accountController = require("../controllers/accountController")
 const regValidate = require("../utilities/account-validation")
+const accntCont = require("../controllers/accountController")
+
+// Deliver account management view
+router.get("/", utilities.handleErrors(accountController.buildManagement))
 
 // Deliver log in view
 router.get("/login", utilities.handleErrors(accountController.buildLogin)) 
@@ -15,14 +19,15 @@ router.get("/register",utilities.handleErrors(accountController.buildRegistratio
 router.post("/register",
     regValidate.registrationRules(),
     regValidate.checkRegData,
-    utilities.handleErrors(accountController.registerAccount))
+    utilities.handleErrors(accountController.registerAccount)
+  )
 
 // Process the login attempt
 router.post(
     "/login",
-    (req, res) => {
-      res.status(200).send('login process')
-    }
+    regValidate.loginRules(),
+    regValidate.checkLoginData,
+    utilities.handleErrors(accntCont.accountLogin)
   )
   
 module.exports = router
